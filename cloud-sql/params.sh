231d519db149b75786ae985371c22f327e8ec511
Amit Einav <amiteinav@google.com>
2019-12-18 16:46:26 +0200

    Update params.sh

---
+++ cloud-sql/params.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export PROJECT_ID=amiteinav-sandbox
+export REGION=europe-west1
+export INSTANCE_ID=task-navigator-mysql-1
+export VPC_NETWORK_NAME=privatenet
+export EU_WST=privatenet-europe-west1
+export EMAIL=amiteinav@google.com
+export MGMT_VM=mgmt-instance-1
+export SERVICE_ACCOUNT=mgmt-instance-sa@task-navigator.iam.gserviceaccount.com
