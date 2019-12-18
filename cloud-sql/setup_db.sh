4af670690c482d6a94df0d6eb27e1a2c5e2d65ab
Amit Einav <amiteinav@google.com>
2019-12-18 22:53:22 +0200

    Delete setup_db.sh

--- cloud-sql/setup_db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source params.sh
-
-MYSQLIP=$(gcloud sql instances describe ${INSTANCE_ID} --format="value(ipAddresses.ipAddress)")
-mysql --host=$MYSQLIP --user=root --password --verbose < recreate_db.sql
