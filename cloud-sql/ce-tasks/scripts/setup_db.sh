34ad5c36c6b247a45269e2549973b0858ce659a3
Amit Einav <amiteinav@google.com>
2019-12-19 10:08:42 +0200

    Delete setup_db.sh

--- cloud-sql/ce-tasks/scripts/setup_db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source params.sh
-
-MYSQLIP=$(gcloud sql instances describe ${INSTANCE_ID} --format="value(ipAddresses.ipAddress)")
-mysql --host=$MYSQLIP --user=root --password --verbose  < ../sql/db_construct.sql
-
-mysqlimport --local --host=$MYSQLIP --user=root --ignore-lines=1 --fields-terminated-by=',' bts users.csv
