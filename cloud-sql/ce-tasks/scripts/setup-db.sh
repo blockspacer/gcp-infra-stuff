a9eb5b40fcad3ef9d303a3eadda7de19b90d139c
Amit Einav <amiteinav@google.com>
2019-12-19 10:10:15 +0200

    Update setup-db.sh

--- cloud-sql/ce-tasks/scripts/setup-db.sh
+++ cloud-sql/ce-tasks/scripts/setup-db.sh
@@ -2,11 +2,11 @@
 
 source params.sh
 
-cat $PASSWD
+pass=cat $PASSWD
 
 MYSQLIP=$(gcloud sql instances describe ${INSTANCE_ID} --format="value(ipAddresses.ipAddress)")
 mysql --host=$MYSQLIP --user=root --password --verbose  < ../sql/db_construct.sql
 
-mysqlimport --local --host=$MYSQLIP --user=root --ignore-lines=1 --fields-terminated-by=',' bts roles.csv users.csv
+mysqlimport --local --host=$MYSQLIP --user=root --ignore-lines=1 --password $pass --fields-terminated-by=',' bts roles.csv users.csv
 
 
