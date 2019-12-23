33153271371c091a47b3a8ee5624ace8309b3509
Amit Einav <amiteinav@google.com>
2019-12-23 18:19:04 +0200

    Update convert-csv-to-tables.py

--- cloud-sql/ce-tasks/scripts/convert-csv-to-tables.py
+++ cloud-sql/ce-tasks/scripts/convert-csv-to-tables.py
@@ -5,8 +5,8 @@ from io import open
 import sys, getopt
 import csv
 
-def get_user_name(name):
-    with open('../csv/USERS.csv', newline='') as csvfile:
+def get_user_name(name,userscsv):
+    with open(userscsv, newline='') as csvfile:
             filereader = csv.reader(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             next(filereader, None)  # skip the 1st line
             for row in filereader:
@@ -14,8 +14,8 @@ def get_user_name(name):
                 if ( name == full_name  ):
                     return row[1]
 
-def get_status_code(status):
-    with open('../csv/STATUS_TYPES.csv', newline='') as csvfile:
+def get_status_code(status,csv):
+    with open(statuscsv, newline='') as csvfile:
             filereader = csv.reader(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             next(filereader, None)  # skip the 1st line
             for row in filereader:
@@ -23,40 +23,54 @@ def get_status_code(status):
                     return row[0]
 
 def main(argv,script):
+
+    userscsv='../csv/USERS.csv'
+    statuscsv='../csv/STATUS_TYPES.csv'
+    requestscsv='../csv/REQUESTS.csv'
+    customerscsv='../csv/CUSTOMERS.csv'
+
     inputfile = ''
     outputfile = ''
     try:
-        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+        opts, args = getopt.getopt(argv,"hi:",["ifile="])
     except getopt.GetoptError:
-        print (script + ' -i <inputfile> -o <outputfile>')
+        print (script + ' -i <inputfile>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print ('test.py -i <inputfile> -o <outputfile>')
+            print ('test.py -i <inputfile>')
             sys.exit()
         elif opt in ("-i", "--ifile"):
             inputfile = arg
             if (not path.exists(inputfile)):
                 print ('file ' + inputfile + ' does not exist')
                 sys.exit(2)
-        elif opt in ("-o", "--ofile"):
-            outputfile = arg
 
     if (inputfile == ''):
-        print (script + ' -i <inputfile> -o <outputfile>')
+        print (script + ' -i <inputfile>')
         sys.exit(2)
 
-    with open(outputfile, 'w', newline='') as csvfileout, open(inputfile, newline='') as csvfile:
-        fieldnames = ['REQUEST_ID','REQUEST_INFORMATION','REQUESTOR_ID','REQUEST_OWNER','STATUS_ID','CUSTOMER_ID','OPP_ID','CREATED','LAST_UPDATE','DEAL_YEARS','OPP_SIZE']
-        csv_writer = csv.DictWriter(csvfileout, fieldnames=fieldnames)
+    with open(requestscsv, 'w', newline='') as csvfileout, open(inputfile, newline='') as csvfile, open (customerscsv,'w',newline='') as custcsvfileout:
+        
+        # setting up the requests output file
+        request_fieldnames = ['REQUEST_ID','REQUEST_INFORMATION','REQUESTOR_ID','REQUEST_OWNER','STATUS_ID','CUSTOMER_ID','OPP_ID','CREATED','LAST_UPDATE','DEAL_YEARS','OPP_SIZE']
+        csv_writer = csv.DictWriter(csvfileout, fieldnames=request_fieldnames)
         csv_writer.writeheader()
+
+        # setting up the customers output file
+        customers_fieldnames = ['CUSTOMER_ID','CUSTOMER_DESCRIPTION']
+        cust_csv_writer = csv.DictWriter(custcsvfileout,fieldnames=customers_fieldnames)
+        cust_csv_writer.writeheader()
+        cust_index = 0
         
         filereader = csv.reader(csvfile, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         next(filereader, None)  # skip the 1st line
         next(filereader, None)  # skip the 2nd line
+
         for row in filereader:
                 
-            line = {}
+            reqline = {}
+            custline = {}
             skip_row = False
                 
             fsr=row[0]
@@ -78,14 +92,21 @@ def main(argv,script):
                 skip_row = True
 
             if (not skip_row ):
-                line['REQUEST_INFORMATION'] = description
-                line['REQUESTOR_ID'] = get_user_name(fsr)
-                line['REQUEST_OWNER'] = get_user_name(ce_assigned)
-                line['STATUS_ID'] = get_status_code(status)
-                line['CUSTOMER_ID'] = customer
-                line['OPP_ID'] = sfopp
-                line['CREATED'] = created
-                csv_writer.writerow(line)
+                reqline['REQUEST_INFORMATION'] = description
+                reqline['REQUESTOR_ID'] = get_user_name(fsr,userscsv)
+                reqline['REQUEST_OWNER'] = get_user_name(ce_assigned,userscsv)
+                reqline['STATUS_ID'] = get_status_code(status,statuscsv)
+                reqline['CUSTOMER_ID'] = customer
+                reqline['OPP_ID'] = sfopp
+                reqline['CREATED'] = created
+                csv_writer.writerow(reqline)
+            
+            if (customer != '' ):
+                custline['CUSTOMER_ID'] = cust_index
+                custline['CUSTOMER_DESCRIPTION'] = customer
+                cust_index = cust_index + 1
+                cust_csv_writer.writerow(custline)
+
 
 if __name__ == "__main__":
    main(sys.argv[1:],sys.argv[0])
