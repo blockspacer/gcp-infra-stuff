850388363ba149b38f1330e8bbb6612590bb5998
Amit Einav <amiteinav@google.com>
2019-12-24 12:11:15 +0200

    Update convert-csv-to-tables.py

--- cloud-sql/ce-tasks/scripts/convert-csv-to-tables.py
+++ cloud-sql/ce-tasks/scripts/convert-csv-to-tables.py
@@ -110,7 +110,8 @@ def main(argv,script):
                 reqline['CREATED'] = created
                 csv_writer.writerow(reqline)
             
-            if (customer != '' and get_customer_id(customer,customerscsv) != 0):
+            print ('customer id: ' + get_customer_id(customer,customerscsv) + 'customer: ' + customer)
+            if (customer != '' and (get_customer_id(customer,customerscsv) == 0)):
                 custline['CUSTOMER_ID'] = cust_index
                 custline['CUSTOMER_DESCRIPTION'] = customer
                 cust_index = cust_index + 1
