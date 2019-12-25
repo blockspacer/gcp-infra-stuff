ab225f7c6fff2fd1dcb81c5ec31f0bfa1359fcdb
Amit Einav <amiteinav@google.com>
2019-12-25 23:56:58 +0200

    Update query-db.sh

--- cloud-sql/ce-tasks/scripts/query-db.sh
+++ cloud-sql/ce-tasks/scripts/query-db.sh
@@ -4,7 +4,11 @@ source params.sh
 
 pass=`cat $PASSWD`
 
-MYSQLIP=$(gcloud sql instances describe ${INSTANCE_ID} --format="value(ipAddresses.ipAddress)")
+if [ $1 != "" ] ; then
+    MYSQLIP=$1
+else
+    MYSQLIP=$(gcloud sql instances describe ${INSTANCE_ID} --format="value(ipAddresses.ipAddress)")
+fi
 
 mysql --host=$MYSQLIP --user=root --password=$pass bts  < ../sql/query_entire_db.sql
 
