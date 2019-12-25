ec95390de0c1a98b89575a5ff0989a23661fb691
Amit Einav <amiteinav@google.com>
2019-12-25 23:54:28 +0200

    Update setup-db.sh

--- cloud-sql/ce-tasks/scripts/setup-db.sh
+++ cloud-sql/ce-tasks/scripts/setup-db.sh
@@ -16,6 +16,15 @@ fi
 pass=`cat $PASSWD`
 MYSQLIP=$(gcloud sql instances describe ${INSTANCE_ID} --format="value(ipAddresses.ipAddress)")
 
+IP1=$(echo $MYSQLIP | awk -F";" '{print $1}')
+IP2=$(echo $MYSQLIP | awk -F";" '{print $2}')
+
+if [ $IP2 != "" ] ; then
+    MYSQLIP=${IP2}
+else
+    MYSQLIP=${IP1}
+fi
+
 mysql --host=$MYSQLIP --user=root --password=$pass bts  < ${SQLDIR}/db_construct.sql
 
 #table=REQUESTS
