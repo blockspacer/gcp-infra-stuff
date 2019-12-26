9030572796835042377a8bb455cc5de9940eb49b
Amit Einav <amiteinav@google.com>
2019-12-26 18:36:16 +0200

    Create setup-non-managed-db.sh

---
+++ cloud-sql/ce-tasks/scripts/setup-non-managed-db.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+git pull
+
+source params.sh
+
+gsutil -m cp gs://task-navigator-files/csv/* ${CSVDIR}
+
+CE_TASK_TSV=${CSVDIR}/CE-tasks.tsv
+ls ${CE_TASK_TSV} > /dev/null 2>&1
+if [ $? -eq 0 ] ; then
+    echo "populating information"
+    python3 convert-csv-to-tables.py  -i ${CE_TASK_TSV}
+fi
+
+pass=`cat $PASSWD`
+MYSQLIP=`cat $IPFILE`
+
+mysql --host=$MYSQLIP --user=$USERNAME --password=$pass bts  < ${SQLDIR}/db_construct.sql
+
+tables="ROLES SKILL_MAPPINGS SKILL_TYPES STATUS_TYPES TASK_TYPES USERS CUSTOMERS KNOWLEDGE_MAPPINGS QUEUES REQUESTS TASKS"
+
+for table in $tables ; do
+    if [ -f $CSVDIR/${table}.csv ] ; then
+        echo "Now with table: $table"
+        mysql --host=$MYSQLIP --user=$USERNAME --password=$pass --execute="USE BTS ; LOAD DATA LOCAL INFILE '$CSVDIR/${table}.csv' INTO TABLE ${table} FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' IGNORE 1 LINES ; SHOW WARNINGS"
+    fi
+done
+
+bash query-db.sh ${MYSQLIP}
+
+
