9256446261ec95ca8a03646f7f75df93394a0f39
Amit Einav <amiteinav@google.com>
2019-12-26 18:36:08 +0200

    Update params.sh

--- cloud-sql/ce-tasks/scripts/params.sh
+++ cloud-sql/ce-tasks/scripts/params.sh
@@ -3,12 +3,15 @@
 export PROJECT_ID=amiteinav-sandbox
 export REGION=europe-west1
 export INSTANCE_ID=task-navigator-mysql-1
+export SELF_MANAGED_INSTANCE_ID=self_managed_mysql
 export VPC_NETWORK_NAME=privatenet
 export EU_WST=privatenet-europe-west1
 export EMAIL=amiteinav@google.com
 export MGMT_VM=mgmt-instance-1
 export SERVICE_ACCOUNT=mgmt-instance-sa@task-navigator.iam.gserviceaccount.com
 export PASSWD=${INSTANCE_ID}_password.txt
+export IPFILE=${INSTANCE_ID}_ip.txt
 export CSVDIR=../csv/
 export SQLDIR=../sql/
+export USERNAME=ce-tasks-admin
 
