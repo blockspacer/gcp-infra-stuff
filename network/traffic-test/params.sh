7aab64910ac86354b1a5348f3fa792881a08f94f
Amit Einav <amiteinav@google.com>
2020-01-07 01:09:58 +0200

    Update params.sh

--- network/traffic-test/params.sh
+++ network/traffic-test/params.sh
@@ -2,7 +2,7 @@ export PROJECT_ID=`gcloud config get-value project`
 export PROJECT_NUMBER=`gcloud projects describe ${PROJECT_ID} --format="csv(projectNumber)" | tail -1`
 echo "Project ID: ${PROJECT_ID}; Project number: ${PROJECT_NUMBER}"
 
-export SERVICE_ACOUNT=${PROJECT_NUMBER}-compute@developer.gserviceaccount.com
+export SERVICE_ACCOUNT=${PROJECT_NUMBER}-compute@developer.gserviceaccount.com
 export MACHINE_TYPE=n1-standard-4
 
 export NETWORK_NAME=network-latency-test-vpc
