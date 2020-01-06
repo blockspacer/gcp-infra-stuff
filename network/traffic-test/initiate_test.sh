082ae7cc9767763e125912ea7efc683b6f95ca2f
Amit Einav <amiteinav@google.com>
2020-01-07 01:59:30 +0200

    Update initiate_test.sh

--- network/traffic-test/initiate_test.sh
+++ network/traffic-test/initiate_test.sh
@@ -7,13 +7,17 @@ source params.sh
 curr_host=`hostname`
 sizes="56 504 1492 4492 15492"
 
-for target in `gcloud compute instances list | grep "\-nw-test-vm" | awk '{print $5}'` ; do
+gcloud compute instances list | grep "\-nw-test-vm" | awk '{print $1" "$5}' > /tmp/file$$
+
+for target in `cat /tmp/file$$ | awk '{print $2}'` ; do
+
+    target_host=`grep $target /tmp/file$$ | awk '{print $1}'`
 
     for size in $sizes ; do
         sudo ping -i0.05  $target -c 100 -s $size \
         | grep -v "\---" | grep -v pipe | grep -v "\%" \
-        | grep -v "PING" | uniq | awk '{print $1 "," $4",source_host,"$7}' \
-        | sed "s/://g" | sed "s/time=//g" | sed "s/source_host/${curr_host}/g" | grep -v rtt | grep -v "\,,"
+        | grep -v "PING" | uniq | awk '{print $1 ",target_host",source_host,"$7}' \
+        | sed "s/://g" | sed "s/time=//g" | sed "s/source_host/${curr_host}/g" | grep -v rtt | grep -v "\,," | sed "s/target_host/${target_host}/g"
     done
 
 done
