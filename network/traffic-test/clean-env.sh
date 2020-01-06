2008f19b5e2078ad632ab44255e51328fb708279
Amit Einav <amiteinav@google.com>
2020-01-07 01:02:05 +0200

    Update clean-env.sh

--- network/traffic-test/clean-env.sh
+++ network/traffic-test/clean-env.sh
@@ -2,7 +2,6 @@
 
 source params.sh
 
-
 echo "Starting with network testing setup script"
 
 
@@ -12,6 +11,6 @@ if [ "${instances_to_delete}" != "" ] ; then
     for instnace in ${instances_to_delete} ; do
         zone=`echo $instance | sed "s/-nw-test-vm//g"`
         gcloud compute instances delete $instance --quiet --delete-disks=all --zone $zone
-    fi
+    done
 
 fi
