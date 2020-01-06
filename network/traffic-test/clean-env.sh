2927a80e479ae67ce67b79a4e3f94f48bf080d9d
Amit Einav <amiteinav@google.com>
2020-01-07 01:07:38 +0200

    Update clean-env.sh

--- network/traffic-test/clean-env.sh
+++ network/traffic-test/clean-env.sh
@@ -11,11 +11,8 @@ if [ "${instances_to_delete}" != "" ] ; then
     for instance in ${instances_to_delete} ; do
         zone=`echo $instance | sed "s/-nw-test-vm//g"`
         echo "gcloud compute instances delete $instance --quiet --delete-disks=all --zone $zone"
-        gcloud compute instances delete $instance --quiet --delete-disks=all --zone $zone 
-
-
+        gcloud compute instances delete $instance --quiet --delete-disks=all --zone $zone  &
     done
-
 fi
 
 gcloud compute instances list | grep "\-nw-test-vm" | awk '{print $1}'
