9b2325b24ac86cadc5cdbd869c6244a2db6e4668
Amit Einav <amiteinav@google.com>
2020-01-07 13:22:35 +0200

    Update remote-ssh-execute.sh

--- network/traffic-test/remote-ssh-execute.sh
+++ network/traffic-test/remote-ssh-execute.sh
@@ -4,12 +4,12 @@ source params.sh
 
 # $1 is the command to run across all servers 
 # Example:
-# $0 "cd gcp-infra-stuff/network/traffic-test; bash ping_test.sh"
+# $0 "cd ~/gcp-infra-stuff/network/traffic-test; git pull ; bash ping_test.sh"
 
 if [ "${1}" != "" ] ; then
     command=$1
 else
-    command="hostname; sudo apt-get update -y ; sudo apt-get install git dnsutils curl iperf iperf3 -y ; git clone https://github.com/amiteinav/gcp-infra-stuff.git"
+    command="hostname; sudo apt-get update -y ; sudo apt-get install git dnsutils curl iperf iperf3 inetutils-traceroute -y ; git clone https://github.com/amiteinav/gcp-infra-stuff.git"
 fi
 
 for curr_ZONE in `gcloud compute instances list --project ${PROJECT_ID} | grep nw-test-vm | awk '{print $2}'` ; do
