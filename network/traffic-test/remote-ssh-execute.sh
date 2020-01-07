acb2b53a33810f60c2cf83478bb765d47bc18145
Amit Einav <amiteinav@google.com>
2020-01-07 11:21:06 +0200

    Update remote-ssh-execute.sh

--- network/traffic-test/remote-ssh-execute.sh
+++ network/traffic-test/remote-ssh-execute.sh
@@ -4,9 +4,11 @@ source params.sh
 
 for ZONE in `gcloud compute instances list --project ${PROJECT_ID} | grep nw-test-vm | awk '{print $2}'` ; do
 
-    gcloud compute ssh ${ZONE}-vm \
+    echo "Now eith ${ZONE}-nw-test-vm"
+
+    gcloud compute ssh ${ZONE}-nw-test-vm \
     --command 'sudo apt-get update -y ; sudo apt-get install git dnsutils curl iperf iperf3 -y ; git clone https://github.com/amiteinav/gcp-infra-stuff.git'  \
     --project ${PROJECT_ID} \
-    --zone ${ZONE}
+    --zone ${ZONE} &
 
 done
