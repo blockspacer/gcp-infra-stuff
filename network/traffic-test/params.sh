2a64ab8e4f1663ef503b79cbc3f0859079885f8d
Amit Einav <amiteinav@google.com>
2020-01-27 18:31:36 +0200

    Update params.sh

--- network/traffic-test/params.sh
+++ network/traffic-test/params.sh
@@ -6,3 +6,14 @@ export SERVICE_ACCOUNT=${PROJECT_NUMBER}-compute@developer.gserviceaccount.com
 export MACHINE_TYPE=n1-standard-4
 
 export NETWORK_NAME=network-latency-test-vpc
+
+
+https://storage.googleapis.com/amiteinav-pics/mando/mando1.mkv
+https://storage.googleapis.com/amiteinav-pics/mando/mando2.mkv
+https://storage.googleapis.com/amiteinav-pics/mando/mando3.mkv
+https://storage.googleapis.com/amiteinav-pics/mando/mando4.mkv
+https://storage.googleapis.com/amiteinav-pics/mando/mando5.mkv
+https://storage.googleapis.com/amiteinav-pics/mando/mando6.mkv
+https://storage.googleapis.com/amiteinav-pics/mando/mando7.mkv
+https://storage.googleapis.com/amiteinav-pics/mando/mando8.mkv
+https://storage.googleapis.com/amiteinav-pics/mando/OUATIH.mp4
