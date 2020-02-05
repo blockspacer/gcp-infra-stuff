07643095d70557b5795df9afb24479cfc1fd7deb
Amit Einav <amiteinav@google.com>
2020-02-05 18:57:26 +0200

    Delete run-the-docker.sh

--- gke/web-app-python/run-the-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "docker build . -t gcr.io/amiteinav-sandbox/simpleapp"
-docker build . -t gcr.io/amiteinav-sandbox/simpleapp
-echo "docker push gcr.io/amiteinav-sandbox/simpleapp"
-docker push gcr.io/amiteinav-sandbox/simpleapp 
-echo "docker run -p 8000:8000 gcr.io/amiteinav-sandbox/simpleapp"
-docker run -p 8000:8000 gcr.io/amiteinav-sandbox/simpleapp
