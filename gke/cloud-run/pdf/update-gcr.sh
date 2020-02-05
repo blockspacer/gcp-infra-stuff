e4908b7bfa690b50c410c7c6524e519e34d490bb
Amit Einav <amiteinav@google.com>
2020-02-05 18:34:32 +0200

    Delete update-gcr.sh

--- gke/cloud-run/pdf/update-gcr.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "docker build . -t gcr.io/amiteinav-sandbox/pdf-app-immersion"
-docker build . -t gcr.io/amiteinav-sandbox/pdf-app-immersion
-
-echo "docker push gcr.io/amiteinav-sandbox/pdf-app-immersion"
-docker push gcr.io/amiteinav-sandbox/pdf-app-immersion
