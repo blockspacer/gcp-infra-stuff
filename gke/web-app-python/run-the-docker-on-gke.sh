8a2111df41be6ae40800925bb83a77bbdbd1947f
Amit Einav <amiteinav@google.com>
2020-02-06 20:44:00 +0200

    Update run-the-docker-on-gke.sh

--- gke/web-app-python/run-the-docker-on-gke.sh
+++ gke/web-app-python/run-the-docker-on-gke.sh
@@ -2,13 +2,26 @@
 
 bash ../auth_cluster.sh
 
+NAME=simpleapp
+PORT=8000
+
 echo "docker build . -t gcr.io/amiteinav-sandbox/simpleapp"
 docker build . -t gcr.io/amiteinav-sandbox/simpleapp
 echo "docker push gcr.io/amiteinav-sandbox/simpleapp"
 docker push gcr.io/amiteinav-sandbox/simpleapp 
 
-kubectl run simpleapp --image=gcr.io/amiteinav-sandbox/simpleapp --replicas=2 --port=8000
-kubectl expose deployment simpleapp --port=8000 --type=LoadBalancer
-kubectl get services --watch
+kubectl delete svc $NAME
+kubectl delete deployment $NAME
 
+echo "kubectl apply -f simpleapp.yaml"
+kubectl apply -f simpleapp.yaml
+
+echo "Getting service IP"
+while [ "${IP}" == "" ] ; do
+    IP=`kubectl get services $NAME --output jsonpath='{.status.loadBalancer.ingress[0].ip}'`
+    sleep 2
+    echo -n '.'
+done;
+echo "Now running curl http://${IP}:${PORT}"
+curl http://${IP}:${PORT}
 
